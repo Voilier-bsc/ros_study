d161faeed27cfe22317db72407b6f19abf476cbd
Cordin <qjatjd0307@naver.com>
2021-07-06 15:42:25 +0900

    change service tutorials

--- ros_tutorials_service/src/service_server.cpp
+++ ros_tutorials_service/src/service_server.cpp
@@ -1,9 +1,42 @@
 #include "ros/ros.h"
 #include "ros_tutorials_service/SrvTutorial.h"
 
-bool calculation(ros_tutorials_service::SrvTutorial::Request &req, ros_tutorials_service::SrvTutorial::Response &res){
-    res.result = req.a + req.b;
+#define PLUS        1
+#define MINUS       2
+#define MULTI       3
+#define DIVIDE      4
 
+int g_operator = PLUS;
+
+bool calculation(ros_tutorials_service::SrvTutorial::Request &req, ros_tutorials_service::SrvTutorial::Response &res){
+    
+    
+    switch (g_operator){
+        case PLUS:
+            res.result = req.a + req.b;
+            break;
+
+        case MINUS:
+            res.result = req.a - req.b;
+            break;
+
+        case MULTI:
+            res.result = req.a * req.b;
+            break;
+
+        case DIVIDE:
+            if(req.b == 0){
+                res.result = 0;
+                break;
+            }
+            res.result = req.a / req.b;
+            break;
+
+        default:
+            res.result = req.a + req.b;
+            break;
+    }
+    ROS_INFO("g_operator is %d", g_operator);
     ROS_INFO("request : x = %ld, y = %ld", (long int)req.a, (long int)req.b);
     ROS_INFO("sending back response : %ld", (long int)res.result);
 
@@ -14,11 +47,19 @@ int main(int argc, char **argv){
     ros::init(argc,argv,"service_server");
     ros::NodeHandle nh;
 
+    nh.setParam("calculation_method", PLUS); //파라미터 초기 설정
+
     //서비스 선언, 서비스 요청이 생기면 calculation 함수 실행
     ros::ServiceServer ros_tutorial_service_server = nh.advertiseService("ros_tutorial_srv",calculation);
     ROS_INFO("ready srv server");
-
-    ros::spin(); // 서비스 요청 대기
+    ros::Rate r(10);
+    
+    while(1)
+    {
+        nh.getParam("calculation_method",g_operator);
+        ros::spinOnce(); //callback 함수 처리 루틴
+        r.sleep();
+    }
 
     return 0;
 }
